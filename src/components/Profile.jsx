34556dd10bf04ca4ffe8a5ea17490889556f38d5
Lawyer0901 <105603303+Lawyer0901@users.noreply.github.com>
2022-12-14 21:51:02 +0200

    changed src

---
+++ src/components/Profile.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+
+export default function Profile(props) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={props.avatar} alt={props.username} className="avatar" />
+        <p className="name">{props.username}</p>
+        <p className="tag">{props.tag}</p>
+        <p className="location">{props.location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{props.stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{props.stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{props.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
